473404f45460491d49cbbcbea992fa9f15d4f88e
faruken <faakgul@gmail.com>
2012-09-27 21:13:35 +1000

    Compatibility with the latest brubeck version.

--- examples/brubeck/main.py
+++ examples/brubeck/main.py
@@ -3,6 +3,7 @@
 
 import logging
 from brubeck.templating import load_jinja2_env
+from brubeck.connections import Mongrel2Connection
 from brubeck.request_handling import Brubeck
 from brubeck.templating import Jinja2Rendering
 
@@ -13,7 +14,7 @@ class IndexHandler(Jinja2Rendering):
 
 
 config = {
-  'mongrel2_pair': ('ipc://127.0.0.1:9999', 'ipc://127.0.0.1:9998'),
+  'mongrel2_pair': Mongrel2Connection('ipc://127.0.0.1:9999', 'ipc://127.0.0.1:9998'),
   'handler_tuples': [(r'^/$', IndexHandler)],
   'log_level': logging.DEBUG,
   'template_loader': load_jinja2_env('templates'),
