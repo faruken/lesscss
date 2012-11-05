b86dd9e900945461174039ff44639e51693df09a
faruken <faakgul@gmail.com>
2012-11-06 05:39:56 +1100

    Update version

--- setup.py
+++ setup.py
@@ -57,7 +57,7 @@ from setuptools import (setup, find_packages)
 setup(
 
     name='lesscss',
-    version='0.1.4',
+    version='0.1.5',
     url='http://github.com/faruken/lesscss',
     license='BSD',
     author='Faruk Akgul',
